7322ed82241255d6f3e3528241e3b1e8b35f2fa1
hyosun <andy703744@gamil.com>
2022-11-19 15:52:47 +0900

    컬러피커 추가

---
+++ script.js
@@ -0,0 +1,41 @@
+let picker;
+let color = '#ffffff';
+const defaults = {
+  color: color,
+  container: document.getElementById('color_picker'),
+  onChange: function(color) {
+    updateColor(color);
+  },
+  swatchColors: ['#E9ACAC', '#D86C6C', '#B73232', '#AD0707', '#611B1B'],
+};
+
+function initPicker(options) {
+  options = Object.assign(defaults, options);
+  picker = new EasyLogicColorPicker(options);
+}
+
+function updateColor(value) {
+  color = value;
+  const $color = document.querySelector('.sample__color');
+  const $code = document.querySelector('.sample__code');
+  $code.innerText = color;
+  $color.style.setProperty('--color', color);
+}
+
+function onChangeType(e) {
+  picker.setType(e.value);
+}
+
+function copy(colorType){
+  const colorValue = document.querySelector('.sample__code').innerText;
+  const textValue = document.querySelector('.inputText').value;
+  if(colorType == 'text'){
+    const colorTextChangeResult = `\\color{${colorValue}}\\textsf{${textValue}}`;
+    navigator.clipboard.writeText(colorTextChangeResult);
+  }
+}
+
+window.onload = function() {
+  initPicker();
+  updateColor(color);
+};
