747bf7c7a85bd67ad3f83782ae1cd2ee0d74ca7d
hyosun <andy703744@gamil.com>
2022-11-20 18:54:55 +0900

    const로 수정

--- script.js
+++ script.js
@@ -9,12 +9,12 @@ const defaults = {
   swatchColors: ['#E9ACAC', '#D86C6C', '#B73232', '#AD0707', '#611B1B'],
 };
 
-function initPicker(options) {
-  options = Object.assign(defaults, options);
-  picker = new EasyLogicColorPicker(options);
-}
+const initPicker = (options) => {
+   options = Object.assign(defaults, options);
+   picker = new EasyLogicColorPicker(options);
+};
 
-function updateColor(value) {
+const updateColor = (color) => {
   color = value;
   const $color = document.querySelector('.sample__color');
   const $code = document.querySelector('.sample__code');
@@ -22,11 +22,11 @@ function updateColor(value) {
   $color.style.setProperty('--color', color);
 }
 
-function onChangeType(e) {
+const onChangeType = (e) => {
   picker.setType(e.value);
 }
 
- function copyText()  {
+const copyText = () => {
   const colorValue = document.querySelector('.sample__code').innerText;
   const textValue = document.querySelector('.input-text').value;
     const colorTextChangeResult = `\\color{${colorValue}}\\textsf{${textValue}}`;
@@ -38,10 +38,9 @@ function onChangeType(e) {
     el.select();
     document.execCommand("copy");
     document.body.removeChild(el);
-    
 }
 
-window.onload = function() {
+window.onload = () => {
   initPicker();
   updateColor(color);
-};
+}
