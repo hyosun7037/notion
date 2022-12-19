ba56823fab2557d61937c3c64cf0af9b4e55b3ce
hyosun <andy703744@gamil.com>
2022-12-19 20:38:04 +0900

    2023년도 추가

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const defaults = {
   container: document.getElementById('color_picker'),
   onChange: function(color) {
     updateColor(color);
+    document.querySelector('.input-text').style.color = color;
+    document.querySelector('in')
   },
   swatchColors: ['#E9ACAC', '#D86C6C', '#B73232', '#AD0707', '#611B1B'],
 };
@@ -20,31 +22,41 @@ function updateColor(value) {
   const $code = document.querySelector('.sample__code');
   $code.innerText = color;
   $color.style.setProperty('--color', color);
+  
 }
 
 function onChangeType(e) {
   picker.setType(e.value);
 }
 
- function copyText()  {
+ function copyText(){
   const colorValue = document.querySelector('.sample__code').innerText;
+  console.log(colorValue)
   const textValue = document.querySelector('.input-text').value;
-    const colorTextChangeResult = `\\color{${colorValue}}\\textsf{${textValue}}`;
-    console.log(colorValue);
-    console.log(textValue);
+  const colorTextChangeResult = `\\color{${colorValue}}\\Huge{${textValue}}`;
+  // const colorTextChangeResult = `\\color{${colorValue}}\\textsf{${textValue}}\\Huge`;
+    const bgValue = document.querySelector('.sample__code').innerText;
+    // const colorTextAndBgChangeResult = `\\color{${colorValue}}\\colorbox{${bgValue}}{${textValue}}`
     const el = document.createElement("textarea");
     el.value = colorTextChangeResult;
     document.body.appendChild(el);
     el.select();
     document.execCommand("copy");
-    document.body.removeChild(el);    
+    document.body.removeChild(el);  
 }
+
 
 function blindOpacity(){
   const opacity = document.querySelector('.el-cp-slider--alpha');
   opacity.style.display = 'none';
-  console.log(opacity.style.display);
 }
+
+
+const formatter = new Intl.RelativeTimeFormat('en', { numeric: 'auto' });
+formatter.format(1, 'day'); // tomorrow
+formatter.format(2, 'day'); // in 2 days
+formatter.format(-1, 'day'); // yesterday
+formatter.format(-2, 'day'); // 2 datys ago
 
 
 window.onload = function() {
