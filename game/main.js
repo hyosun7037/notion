0737ce5c5f634224d41df7821fc7155396a6ff93
hyosun <andy703744@gamil.com>
2023-01-28 19:21:29 +0900

    game setting

---
+++ game/main.js
@@ -0,0 +1,68 @@
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth - 100;
+canvas.height = window.innerHeight - 100;
+
+// Draw a rectangle
+// ctx.fillStyle = 'green';
+// ctx.fillRect(10, 10, 100, 100);
+
+// 캐릭터 등장 시, 속성 Object 자료에 정리해두면 편리
+var character = { 
+    x : 10,
+    y : 200,
+    width: 50,
+    height: 50,
+    draw(){
+        ctx.fillStyle = 'green';
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+character.draw();
+
+
+// 장애물 (근데 장애물들은 width, height 이런게 각각 다를 수도 있기 때문에 Class를 활용해서 만들어야 함)
+class Obstacle {
+    constructor(){
+        this.x = 500;
+        this.y = 200;
+        this.width = 50;
+        this.height = 50;
+    }
+    draw(){
+        ctx.fillStyle = 'red';
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+var obstacle = new Obstacle();
+obstacle.draw();
+
+var timer = 0;
+var obstacles = [];
+
+// 1초에 60번 실행
+function frameByFrame(){
+    requestAnimationFrame(frameByFrame);
+    timer++;
+    
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // 1초에 1번씩 장애물 생성
+    if(timer % 60 === 0){
+        var obstacle = new Obstacle();
+        obstacles.push(obstacle);
+    }
+    
+    
+    // 장애물 움직이기
+    for(var i = 0; i < obstacles.length; i++){
+        obstacles[i].x -= 5;
+        obstacles[i].draw();
+    }
+    character.draw();
+}
+
+frameByFrame();
