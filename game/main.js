d1ba1a575665f361ab7ed6e597782e7e9ffc42c8
hyosun <andy703744@gamil.com>
2023-01-28 20:30:55 +0900

    점프 기능 추가

--- game/main.js
+++ game/main.js
@@ -8,19 +8,26 @@ canvas.height = window.innerHeight - 100;
 // ctx.fillStyle = 'green';
 // ctx.fillRect(10, 10, 100, 100);
 
+var player = new Image();
+player.src = 'character.png';
+
 // 캐릭터 등장 시, 속성 Object 자료에 정리해두면 편리
 var character = { 
     x : 10,
     y : 200,
-    width: 50,
-    height: 50,
+    width: 80,
+    height: 90,
     draw(){
         ctx.fillStyle = 'green';
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        // ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.drawImage(player, this.x, this.y, this.width, this.height);  
     }
 }
 
 character.draw();
+
+var img = new Image();
+img.src = 'character.png';
 
 
 // 장애물 (근데 장애물들은 width, height 이런게 각각 다를 수도 있기 때문에 Class를 활용해서 만들어야 함)
@@ -28,12 +35,14 @@ class Obstacle {
     constructor(){
         this.x = 500;
         this.y = 200;
-        this.width = 50;
-        this.height = 50;
+        this.width = 80;
+        this.height = 90;
     }
     draw(){
         ctx.fillStyle = 'red';
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        // hitbox
+        // ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.drawImage(img, this.x, this.y, this.width, this.height);
     }
 }
 
@@ -43,16 +52,17 @@ obstacle.draw();
 var timer = 0;
 var obstacles = [];
 var jumpTimer = 0;
+var animation;
 
 // 1초에 60번 실행
 function frameByFrame(){
-    requestAnimationFrame(frameByFrame);
+    animation = requestAnimationFrame(frameByFrame);
     timer++;
     
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     // 1초에 1번씩 장애물 생성
-    if(timer % 60 === 0){
+    if(timer % 150 === 0){
         var obstacle = new Obstacle();
         obstacles.push(obstacle);
     }
@@ -63,7 +73,10 @@ function frameByFrame(){
         // x좌표가 0미만이면 제거 
         if(obstacles[i].x < 0){
             obstacles.splice(i, 1)};
-        obstacles[i].x -= 5;
+
+        // 충돌체크
+        collisionCheck(character, obstacles[i]);    
+        obstacles[i].x -= 2;
         obstacles[i].draw();
     }
     // character y축으로 점프
@@ -88,6 +101,23 @@ function frameByFrame(){
 }
 
 frameByFrame();
+
+
+// 충돌확인
+function collisionCheck(character, obstacles){
+    // x축 차이
+    var xDiff = obstacles.x - (character.x + character.width);
+    // y축 차이
+    var yDiff = obstacles.y - (character.y + character.height);
+    // x축 차이가 0보다 작고, y축 차이가 0보다 작으면 충돌
+    if(xDiff < 0 && yDiff < 0){
+        // 게임 정지
+        // 화면 깨끗하게 지우기
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        cancelAnimationFrame(animation);
+        alert('Game Over');
+    }
+}
 
 
 
